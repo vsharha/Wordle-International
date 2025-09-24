6bfa6e30d45d5ec04c291db7ac367406af5131b4
Viktor Sharha <vsharha07@gmail.com>
2025-09-24 14:19:12 +0100

    fixed x button location

--- src/ui/AboutScreen.jsx
+++ src/ui/AboutScreen.jsx
@@ -10,29 +10,30 @@ function AboutScreen({open, setIsOpen}) {
 
   return (
     <Modal open={open} onClose={()=>setIsOpen(false)}>
-      <div className= "text-font dark:text-font-dark bg-back dark:bg-back-dark p-5 md:p-10 rounded-lg">
-
-      <div className="relative">
-        <div className="h-10"></div>
-        <CloseButton onClose={() => setIsOpen(false)} />
-      </div>
-        <h1 className="font-bold text-3xl font-serif">How To Play</h1>
-        <h2 className="text-lg">Guess the Wordle in {maxAttempts} tries</h2>
-        <ul className="list-disc pl-5 mt-3">
-          <li>
-            Each guess must be a valid {wordLength}-letter word.
-          </li>
-          <li>
-            The color of the tiles will change to show how close your guess was to the word.
-          </li>
-        </ul>
-        <p className="font-bold mt-3">Examples</p>
-        <DisplayWord word="wordy" skipArray={["correct"]} className="justify-start mt-3" skipWordLen={5}/>
-        <p><span className="font-bold">W</span> is in the word and in the correct spot.</p>
-        <DisplayWord word="light" skipArray={["", "present"]} className="justify-start mt-3" skipWordLen={5}/>
-        <p><span className="font-bold">I</span> is in the word but in the wrong spot.</p>
-        <DisplayWord word="rogue" skipArray={["", "", "", "guessed"]} className="justify-start mt-3" skipWordLen={5}/>
-        <p><span className="font-bold">U</span> is not in the word in any spot.</p>
+      <div className= "text-font dark:text-font-dark bg-back dark:bg-back-dark rounded-lg p-3">
+        <div className="relative">
+          <div className="h-10"></div>
+          <CloseButton onClose={() => setIsOpen(false)} />
+        </div>
+        <div className="p-2 md:p-7">
+          <h1 className="font-bold text-3xl font-serif">How To Play</h1>
+          <h2 className="text-lg">Guess the Wordle in {maxAttempts} tries</h2>
+          <ul className="list-disc pl-5 mt-3">
+            <li>
+              Each guess must be a valid {wordLength}-letter word.
+            </li>
+            <li>
+              The color of the tiles will change to show how close your guess was to the word.
+            </li>
+          </ul>
+          <p className="font-bold mt-3">Examples</p>
+          <DisplayWord word="wordy" skipArray={["correct"]} className="justify-start mt-3" skipWordLen={5}/>
+          <p><span className="font-bold">W</span> is in the word and in the correct spot.</p>
+          <DisplayWord word="light" skipArray={["", "present"]} className="justify-start mt-3" skipWordLen={5}/>
+          <p><span className="font-bold">I</span> is in the word but in the wrong spot.</p>
+          <DisplayWord word="rogue" skipArray={["", "", "", "guessed"]} className="justify-start mt-3" skipWordLen={5}/>
+          <p><span className="font-bold">U</span> is not in the word in any spot.</p>
+        </div>
       </div>
     </Modal>
   );
