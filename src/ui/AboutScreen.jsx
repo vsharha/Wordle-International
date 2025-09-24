2a639fc8e0bc9acbf13dd23bbb4d2e7801f3ca6c
Viktor Sharha <vsharha07@gmail.com>
2025-09-24 14:04:44 +0100

    changed about section

--- src/ui/AboutScreen.jsx
+++ src/ui/AboutScreen.jsx
@@ -16,9 +16,9 @@ function AboutScreen({open, setIsOpen}) {
         <div className="h-10"></div>
         <CloseButton onClose={() => setIsOpen(false)} />
       </div>
-        <h1 className="font-bold text-2xl">How To Play</h1>
+        <h1 className="font-bold text-3xl font-serif">How To Play</h1>
         <h2 className="text-lg">Guess the Wordle in {maxAttempts} tries</h2>
-        <ul className="list-disc pl-5">
+        <ul className="list-disc pl-5 mt-3">
           <li>
             Each guess must be a valid {wordLength} word.
           </li>
@@ -26,8 +26,8 @@ function AboutScreen({open, setIsOpen}) {
             The color of the tiles will change to show how close your guess was to the word.
           </li>
         </ul>
-        <p className="font-bold">Examples</p>
-        <DisplayWord word="wordy" skipArray={["correct"]} className="justify-start"/>
+        <p className="font-bold mt-3">Examples</p>
+        <DisplayWord word="wordy" skipArray={["correct"]} className="justify-start mt-3"/>
         <p><span className="font-bold">W</span> is in the word and in the correct spot.</p>
         <DisplayWord word="light" skipArray={["", "present"]} className="justify-start mt-3"/>
         <p><span className="font-bold">I</span> is in the word but in the wrong spot.</p>
