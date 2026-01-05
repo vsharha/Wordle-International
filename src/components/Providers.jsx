5b871348070dc89e6f0478ebdde62b4ba98491e2
Viktor Sharha <vsharha07@gmail.com>
2026-01-05 15:26:48 +0000

    Add suspense boundary

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -2,9 +2,14 @@
 
 import store from "../slices/store";
 import { Provider } from "react-redux";
+import { Suspense } from "react";
 
 const Providers = ({ children }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+    </Provider>
+  );
 };
 
 export default Providers;
